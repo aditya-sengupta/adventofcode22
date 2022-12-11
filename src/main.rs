61dd538d66f7c56e531053242a09ca9beb0e1d14
aditya-sengupta <adityars@ucsc.edu>
2022-12-11 08:55:02 -0800

    modulo!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// this is a working? solution to part b
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -9,18 +11,18 @@ where P: AsRef<Path>, {
 }
 
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<i64>,
     binaryop: String,
-    rhs: i32,
-    divis: i32,
-    true_target: i32,
-    false_target: i32,
-    ninspect: i32
+    rhs: i64,
+    divis: i64,
+    true_target: i64,
+    false_target: i64,
+    ninspect: i64
 }
 
 impl Monkey {
-    fn operation(&self, worry: i32) -> i32 {
-        let r: i32;
+    fn operation(&self, worry: i64) -> i64 {
+        let r: i64;
         if self.rhs == 0 {
             r = worry;
         } else {
@@ -33,7 +35,7 @@ impl Monkey {
         }
     }
 
-    fn target(&self, worry: i32) -> i32 {
+    fn target(&self, worry: i64) -> i64 {
         if worry.checked_rem(self.divis).unwrap() == 0 {
             return self.true_target
         } else {
@@ -42,23 +44,23 @@ impl Monkey {
     }
 }
 
-fn parse_numbers(words: Vec<&str>) -> Vec<i32> {
-    let mut nums: Vec<i32> = vec![];
+fn parse_numbers(words: Vec<&str>) -> Vec<i64> {
+    let mut nums: Vec<i64> = vec![];
     for i in 2..words.len() {
-        nums.push(words[i][0..2].parse::<i32>().unwrap());
+        nums.push(words[i][0..2].parse::<i64>().unwrap());
     }
     return nums
 }
 
 fn parse_monkeys() -> Vec<Monkey> {
     let mut monkeys: Vec<Monkey> = vec![];
-    let mut items: Vec<i32> = vec![];
+    let mut items: Vec<i64> = vec![];
     let mut binaryop: String = "".to_string();
-    let mut rhs: i32 = 0;
-    let mut divis: i32 = 0;
-    let mut true_target: i32 = 0;
-    let mut false_target: i32 = 0;
-    if let Ok(lines) = read_lines("./day11_test") {
+    let mut rhs: i64 = 0;
+    let mut divis: i64 = 0;
+    let mut true_target: i64 = 0;
+    let mut false_target: i64;
+    if let Ok(lines) = read_lines("./day11") {
         for line in lines { 
             if let Ok(ip) = line {
                 let words: Vec<&str> = ip.split_whitespace().collect();
@@ -67,17 +69,17 @@ fn parse_monkeys() -> Vec<Monkey> {
                         "Starting" => items = parse_numbers(words),
                         "Operation:" => { 
                             binaryop = words[4].to_string();
-                            rhs = match words[5].parse::<i32>() {
+                            rhs = match words[5].parse::<i64>() {
                                 Ok(p) => p,
                                 _error => 0
                             }
                         }
-                        "Test:" => divis = words[3].parse::<i32>().unwrap(),
+                        "Test:" => divis = words[3].parse::<i64>().unwrap(),
                         "If" => {
                             match words[1] {
-                                "true:" => true_target = words[5].parse::<i32>().unwrap(),
+                                "true:" => true_target = words[5].parse::<i64>().unwrap(),
                                 "false:" => {
-                                    false_target = words[5].parse::<i32>().unwrap();
+                                    false_target = words[5].parse::<i64>().unwrap();
                                     let m = Monkey {
                                         items : items.clone(),
                                         binaryop : binaryop.clone(),
@@ -106,13 +108,17 @@ fn main() {
     day11(10000, 1);
 }
 
-fn day11(nr: i32, divworry: i32) {
+fn day11(nr: i64, worrydiv: i64) {
     let mut monkeys = parse_monkeys();
+    let mut f = 1;
+    for m in &monkeys {
+        f *= m.divis;
+    }
     let nm = monkeys.len();
     for _ in 0..nr {
         for i in 0..nm {
             for item in monkeys[i].items.clone() {
-                let w = monkeys[i].operation(item) / divworry;
+                let w = (monkeys[i].operation(item) / worrydiv) % f;
                 let target = monkeys[i].target(w) as usize;
                 monkeys[target].items.push(w);
                 monkeys[i].ninspect += 1;
@@ -121,7 +127,7 @@ fn day11(nr: i32, divworry: i32) {
         }
     }
 
-    let mut business: Vec<i32> = Vec::new();
+    let mut business: Vec<i64> = Vec::new();
     for m in monkeys {
         business.push(m.ninspect);
     }
