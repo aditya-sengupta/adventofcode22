9a9db477a77719c41f638acc9b18d12c112e578d
aditya-sengupta <adityars@ucsc.edu>
2022-12-12 01:42:24 -0800

    day 12 done

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,42 @@ where P: AsRef<Path>, {
 }
 
 fn main() {
-    day12_01();
+    day12(true);
+    day12(false);
 }
 
-fn neighbors(grid: &Vec<Vec<u8>>, vis: &Vec<Vec<bool>>, current: (usize, usize), xm: usize, ym: usize) -> Vec<(usize, usize)> {
+fn neighbors(grid: &Vec<Vec<u8>>, vis: &Vec<Vec<bool>>, current: (usize, usize), xm: usize, ym: usize, up: bool) -> Vec<(usize, usize)> {
     let mut neighbors: Vec<(usize, usize)> = Vec::new();
     if current.0 != 0 { neighbors.push((current.0 - 1, current.1)) }
     if current.0 != ym - 1 { neighbors.push((current.0 + 1, current.1)) }
     if current.1 != 0 { neighbors.push((current.0, current.1 - 1)) }
     if current.1 != xm - 1 { neighbors.push((current.0, current.1 + 1)) }
     let c = grid[current.0][current.1] as u8;
-    neighbors.retain(|n| c > grid[n.0][n.1] || grid[n.0][n.1] - c <= 1);
+    if up {
+        neighbors.retain(|n| c > grid[n.0][n.1] || grid[n.0][n.1] - c <= 1);
+    } else {
+        neighbors.retain(|n| c < grid[n.0][n.1] || c - grid[n.0][n.1] <= 1);
+    }
     neighbors.retain(|n| !vis[n.0][n.1]);
     neighbors
 }
 
-fn day12_01() {
+fn terminate(grid: &Vec<Vec<u8>>, current: (usize, usize), end: (usize, usize), up: bool) -> bool {
+    if up {
+        return current == end
+    } else {
+        return grid[current.0][current.1] == 97 // a
+    }
+}
+
+fn day12(up: bool) {
     let mut grid: Vec<Vec<u8>> = vec![];
-    let mut start = (0, 0);
     let mut end = (0, 0);
-    let mut pq: PriorityQueue<(usize, usize), usize> = PriorityQueue::new();
+    let mut pq: PriorityQueue<(usize, usize), i32> = PriorityQueue::new();
     let s = 'S' as u8;
     let e = 'E' as u8;
-    if let Ok(lines) = read_lines("./day12_test") {
+    let mut start = (0, 0);
+    if let Ok(lines) = read_lines("./day12") {
         let mut i = 0;
         for line in lines { 
             if let Ok(ip) = line {
@@ -41,9 +54,11 @@ fn day12_01() {
                 for (j, b) in ip.as_bytes().iter().enumerate() {
                     let c: u8 = *b;
                     if c == s { 
-                        start = (i, j); grid[i].push('a' as u8);
+                        if up { start = (i, j) }
+                        grid[i].push('a' as u8);
                     } else if c == e { 
-                        end = (i, j); grid[i].push('z' as u8)
+                        if up { end = (i, j) } else { start = (i, j) } 
+                        grid[i].push('z' as u8)
                     } else {
                         grid[i].push(c);
                     }
@@ -55,19 +70,23 @@ fn day12_01() {
     let xm = grid[0].len();
     let ym = grid.len();
     let mut visited: Vec<Vec<bool>> = vec![vec![false; xm]; ym];
-    let mut distance: Vec<Vec<usize>> = vec![vec![usize::MAX; grid[0].len()]; grid.len()];
+    let mut distance: Vec<Vec<i32>> = vec![vec![i32::MAX; grid[0].len()]; grid.len()];
     distance[start.0][start.1] = 0;
     let mut current = start;
-    while current != end {
-        let currdist = distance[current.0][current.1];
-        let neighbors = neighbors(&grid, &visited, current, xm, ym);
+    let mut currdist = distance[current.0][current.1] as i32;
+    while !terminate(&grid, current, end, up) {
+        let neighbors = neighbors(&grid, &visited, current, xm, ym, up);
         for n in neighbors {
             let d = distance[n.0][n.1].min(currdist + 1);
             distance[n.0][n.1] = d;
-            pq.push(n, d);
+            pq.push(n, -d);
         }
         visited[current.0][current.1] = true;
-        current = pq.pop().unwrap().0;
+        match pq.pop() {
+            Some(p) => current = p.0,
+            None => ()
+        }
+        currdist = distance[current.0][current.1] as i32;
     }
-    println!("{}", distance[end.0][end.1]);
+    println!("{}", currdist);
 }
